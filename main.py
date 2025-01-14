16843f39120f0923e27b9330dbbf0f0a9d172f9b
anoopka1739 <anoop.kandampadath@gmail.com>
2025-01-15 00:20:59 +0530

    print todo

---
+++ main.py
@@ -0,0 +1,4 @@
+#Lets build initial steps
+# First lets use print to display something.
+print("Enter todo:")
+#now this is just print statement. How do we take input from user to this so he can enter the to-do list??
